8e49cf8399f51b0e3b72289070610766bdfaf69b
Anna Moraco <annamoraco@gmail.com>
2023-09-21 18:04:32 -0300

    String comparison criteria

--- concatremove.cpp
+++ concatremove.cpp
@@ -1,14 +1,54 @@
 #include<iostream>
 #include<string>
+#include<cstring>
+#include<algorithm>
 using namespace std;
 
 void concatremove (string s, string t, int k)
 {
+
+    cout << endl << "s: " << s.length();
+    cout << endl << "t: " << t.length();
+
     if (k >= t.length() + s.length())
-        cout << endl << "Yes";
+        {
+            cout << endl << "Yes";
+            return;
+        }
 
     if (t.length() == s.length() && k < t.length() + s.length())
-        cout << endl << "No";
+        {
+            cout << endl << "No";
+            return;
+        }
+
+    int concat = 0, remove = 0, comands = 0;
+
+    if (s.length() != t.length())
+    {
+
+        while (strncmp(s.c_str(), t.c_str(), min(t.length(),s.length()) - concat) != 0)
+            concat++;
+
+        remove = s.length() - t.length() + concat ;
+
+        comands = remove + t.length() - (s.length() - remove) ;
+
+    }
+
+    bool extra_comands = (k - comands > 0 && (k - comands)%2 == 0);
+
+    if (comands == k || extra_comands )
+        {
+            cout << endl << "Yes";
+            return;
+        }
+
+    else
+        {
+            cout << endl << "No";
+            return;
+        }
 }
 
 int main()
