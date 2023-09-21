fc5ff2d4c55e2f581dae4c8ddd730a5e15997c54
Anna Moraco <annamoraco@gmail.com>
2023-09-21 16:34:33 -0300

    Initializing project

---
+++ concatremove.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void concatremove ()
+{
+  
+
+}
+
+int main()
+{
+    string s = "", t = "";
+    int k = 0;
+
+    cout << endl << "Type s: ";
+    cin >> s;
+    cout << endl << "Type t: ";
+    cin >> t;
+    cout << endl << "Type k: ";
+    cin >> k;
+
+    concatremove();
+
+}
